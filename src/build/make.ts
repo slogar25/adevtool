b66d1872ff1aaa4df57771219ee7e9a7cb168c9b
Anay Wadhera <anay1018@gmail.com>
2024-11-10 18:41:12 -0500

    Use the correct override key for ro.build.desc
    Change-Id: I42f9da78028e5197c3fafd633076a1510647f4f8

--- src/build/make.ts
+++ src/build/make.ts
@@ -201,7 +201,7 @@ export function serializeDeviceMakefile(mk: DeviceMakefile) {
   // Note that this is reliant on support in the platform/build repository to take effect:
   // https://review.lineageos.org/c/LineageOS/android_build/+/335515
   if (mk.build_desc != undefined && mk.build_desc.trim() !== "") {
-    blocks.push(`PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="${mk.build_desc}"`)
+    blocks.push(`PRODUCT_BUILD_PROP_OVERRIDES += BuildDesc="${mk.build_desc}"`)
   }
 
   if (mk.enforceRros != undefined) {
